e9f237b93e3a9ed3a5989f2e52fd40d1570bdf70
anasyakubu <yakubuanas04@gmail.com>
2025-09-27 05:50:26 +0100

    chore: update code structure for improved readability and maintainability

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,15 @@
-import {Suspense} from 'react'
+import { Suspense } from 'react'
 import Link from 'next/link'
-import {PortableText} from '@portabletext/react'
+import { PortableText } from '@portabletext/react'
 
-import {AllPosts} from '@/app/components/Posts'
+import { AllPosts } from '@/app/components/Posts'
 import GetStartedCode from '@/app/components/GetStartedCode'
 import SideBySideIcons from '@/app/components/SideBySideIcons'
-import {settingsQuery} from '@/sanity/lib/queries'
-import {sanityFetch} from '@/sanity/lib/live'
+import { settingsQuery } from '@/sanity/lib/queries'
+import { sanityFetch } from '@/sanity/lib/live'
 
 export default async function Page() {
-  const {data: settings} = await sanityFetch({
+  const { data: settings } = await sanityFetch({
     query: settingsQuery,
   })
 
@@ -22,21 +22,21 @@ export default async function Page() {
             <div className="relative min-h-[40vh] mx-auto max-w-2xl pt-10 xl:pt-20 pb-30 space-y-6 lg:max-w-4xl lg:px-12 flex flex-col items-center justify-center">
               <div className="flex flex-col gap-4 items-center">
                 <div className="text-md leading-6 prose uppercase py-1 px-3 bg-white font-mono italic">
-                  A starter template for
+                  welcome to my Blog
                 </div>
                 <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter text-black">
                   <Link
                     className="underline decoration-brand hover:text-brand underline-offset-8 hover:underline-offset-4 transition-all ease-out"
-                    href="https://sanity.io/"
+                    href="https://anasyakubu"
                   >
-                    Sanity
+                    Anas
                   </Link>
-                  +
+                  {" "}
                   <Link
                     className="underline decoration-black text-framework underline-offset-8 hover:underline-offset-4 transition-all ease-out"
-                    href="https://nextjs.org/"
+                    href="/"
                   >
-                    Next.js
+                    Unscripted
                   </Link>
                 </h1>
               </div>
@@ -50,7 +50,7 @@ export default async function Page() {
               {settings?.description && <PortableText value={settings.description} />}
               <div className="flex items-center flex-col gap-4">
                 <GetStartedCode />
-                <Link
+                {/* <Link
                   href="https://www.sanity.io/docs"
                   className="inline-flex text-brand text-xs md:text-sm underline hover:text-gray-900"
                   target="_blank"
@@ -65,7 +65,7 @@ export default async function Page() {
                   >
                     <path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V12L17.206 8.207L11.2071 14.2071L9.79289 12.7929L15.792 6.793L12 3H21Z"></path>
                   </svg>
-                </Link>
+                </Link> */}
               </div>
             </div>
           </div>
